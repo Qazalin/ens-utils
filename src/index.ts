68e2714116ed73af61c0142cec65bc7313461aa2
qazalin <ghazal.nzhrzr@gmail.com>
2022-04-25 09:42:40 +0430

    feat: ens metadata

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import { getNFTBalance } from './NFTBalance'
+import { ENSMetadata } from "./types"
+import { getNFTBalance } from "./NFTBalance"
 
-getNFTBalance('0x20a6F27E11EdC08D32C21454b4625EECfB22Bb21').then((r) => {
-    console.log(r)
+getNFTBalance("0x20a6F27E11EdC08D32C21454b4625EECfB22Bb21").then((r) => {
+    const ensNames = r.result.find(
+        (items) => items.name === "Ethereum Name Service"
+    )
+    const metadata: ENSMetadata = JSON.parse(ensNames.metadata)
+    console.log(metadata.name)
 })
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export interface ENSMetadata {
+    name: string
+    description: string
+    url: string
+    image_url: string
+}
